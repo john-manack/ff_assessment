2e9ec0b129e36c49e2451e561ee33ba62b818116
John Manack <jmnck28@gmail.com>
2021-04-19 14:12:04 -0400

    completed assessment-1 in js

---
+++ assessment-1.js
@@ -0,0 +1,21 @@
+// Reasoning: by placing Isosceles last in this order of conditionals, a higher-complexity (nested) conditional statement is avoided by the process of elimination.
+
+'use strict';
+
+const defineTriangle = (side1, side2, side3) => {
+    if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+        return 'Scalene'
+    } if (side1 === side2 && side1 === side3) {
+        return 'Equilateral'
+    } else {
+        return 'Isosceles'
+    }
+}
+
+console.log('Should be Scalene: ', defineTriangle(2,3,4));
+console.log('Should be Isosceles: ', defineTriangle(2,3,3));
+console.log('Should be Equilateral: ',defineTriangle(2,2,2));
+console.log('Should be Scalene: ', defineTriangle(4,3,2));
+console.log('Should be Isosceles: ', defineTriangle(3,2,3));
+console.log('Should be Scalene: ', defineTriangle(2,4,3));
+console.log('Should be Isosceles: ', defineTriangle(3,3,2));
