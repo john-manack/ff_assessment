8a6e6e74fe6630923281fa18d43971bcee7143cf
John Manack <jmnck28@gmail.com>
2021-04-21 16:33:53 -0400

    adds basic file structure for assessment 2

---
+++ assessment-2-cs/src/Assessment2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Pseudo code for contrived linked list:
+// 55-->82-->23-->11-->36-->42-->29-->77-->98-->61-->88
+
+using System;
+
+namespace Assessment2
+{
+    internal class Node {
+        internal int data;
+        internal Node next;
+        public Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
