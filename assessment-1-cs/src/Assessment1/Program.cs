10ef94f7b925cb40c0bb095519db86cdf1822ae7
John Manack <jmnck28@gmail.com>
2021-04-21 14:32:49 -0400

    functioning assessment-1

---
+++ assessment-1-cs/src/Assessment1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] != args[1] && args[0] != args[2] && args[1] != args[2])
+            {
+                Console.WriteLine("Scalene");
+            }
+            else if(args[0] == args[1] && args[0] == args[2])
+            {
+                Console.WriteLine("Equilateral");
+            }
+            else
+            {
+                Console.WriteLine("Isosceles");
+            }
+        }
+    }
+}
